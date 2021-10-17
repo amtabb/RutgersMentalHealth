29037febd6ec7dce88dbb8cd8262726c07a6dfaa
ameerasullah <91222477+ameerasullah@users.noreply.github.com>
2021-10-17 11:53:57 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);

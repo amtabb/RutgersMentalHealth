1c3ed5999bb22b8bb76b3393cc56201bfa17b344
ameerasullah <91222477+ameerasullah@users.noreply.github.com>
2021-11-28 17:43:09 -0500

    Update index.jsx

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -14,16 +14,6 @@ function LandingPage(props) {
     inspiration,
     events,
     searchPlaceholde,
-    anonymous,
-    anonymous2,
-    anonymous3,
-    anonymous4,
-    anonymous5,
-    stressed,
-    text11,
-    text12,
-    text13,
-    text14,
     postAnomyously,
     postButton,
     insertTest,
@@ -59,39 +49,13 @@ function LandingPage(props) {
           </div>
           <div className="main-div-for-user-divs"></div>
           <div className="user-divs">
-            <div className="overlap-group2-1">
-              <div className="rectangle-3"></div>
-            </div>
-            <div className="rectangle-1"></div>
-            <div className="rectangle-1"></div>
-            <div className="rectangle-1"></div>
-            <div className="rectangle-8-1"></div>
+
+
           </div>
-          <div className="users-1">
-            <div className="anonymous-5">{anonymous}</div>
-            <div className="anonymous-6">{anonymous2}</div>
-            <div className="anonymous-7">{anonymous3}</div>
-            <div className="anonymous-8">{anonymous4}</div>
-            <div className="anonymous-9">{anonymous5}</div>
-          </div>
-          <div className="text-boxes-for-users-1">
-            <div className="rectangle-11-1"></div>
-            <div className="rectangle-12-1"></div>
-            <div className="rectangle-13-1"></div>
-            <div className="rectangle-14-1"></div>
-            <div className="rectangle-15-1"></div>
-          </div>
-          <div className="user-texts">
-            <div className="stressed">{stressed}</div>
-            <div className="text-11">{text11}</div>
-            <div className="text-12">{text12}</div>
-            <div className="text-13">{text13}</div>
-            <div className="text-14">{text14}</div>
-          </div>
+
           <div className="overlap-group3-1">
             <div className="maindiv"></div>
             <div className="post-anomyously-1 mulish-bold-black-24px">{postAnomyously}</div>
-            <img className="post-div" src="/img/post-div@2x.svg" />
             <div className="post-button mulish-bold-white-24px">{postButton}</div>
             <div className="textbox-div"></div>
             <div className="insert-test">{insertTest}</div>

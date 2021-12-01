b0866f732222bf74e10c175005525d9c6de87d33
amtabb <87440586+amtabb@users.noreply.github.com>
2021-12-01 12:08:05 -0500

    Add files via upload

---
+++ build/precache-manifest.52fe9a56abc2563022486a52707f4e3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3823f7fd36139c8ab7043175cdfaa3f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9fd328ea9fcecdd93c5",
+    "url": "/static/css/main.d94527a8.chunk.css"
+  },
+  {
+    "revision": "d9248292237631737ee3",
+    "url": "/static/js/2.620f6611.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.620f6611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9fd328ea9fcecdd93c5",
+    "url": "/static/js/main.01f73d0d.chunk.js"
+  },
+  {
+    "revision": "49a98f16552705ade189",
+    "url": "/static/js/runtime-main.5bf90215.js"
+  }
+]);

568bf04e4b36a37c4da79f6f588aef7f79780192
amtabb <87440586+amtabb@users.noreply.github.com>
2021-12-15 21:37:41 -0500

    Delete src directory

--- src/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql')
-const db = mysql.createConnection({
-host: "us-cdbr-east-04.cleardb.com",
-user: "b67965676b9bc1",
-password: "fef70fef",
-database:"`heroku_f7b0b0e3718ae7b`" 
-})
-
-module.exports = db;
